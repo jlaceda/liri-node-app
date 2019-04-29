0f450e1e60cbc3892dac63d57b5fbb829fa3cf80
John Michael Laceda <john.laceda@gmail.com>
2019-04-29 00:55:48 -0700

    adding parameter parsing

--- liri.js
+++ liri.js
@@ -3,34 +3,51 @@ const Spotify = require('node-spotify-api');
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
 
-const commands = [
+// first, process user input
+// sample commands:
+//     node liri.js concert-this '<artist/band name here>'
+//     node liri.js spotify-this-song '<song name here>'
+//     node liri.js movie-this '<movie name here>'
+//     node liri.js do-what-it-says
+
+const validCommands = [
   'concert-this',
   'spotify-this-song',
   'movie-this',
   'do-what-it-says'
 ];
 
-const userCommand = process.argv[2];
+const command = process.argv[2];
 
 // when user gives a command not in the list.
-if (commands.indexOf(userCommand) === -1)
+if (validCommands.indexOf(command) === -1)
 {
   console.error('invalid command');
   return;
 }
 
-console.log(`user command: ${userCommand}`);
+// at this point we have a legit command
+console.log(`command: ${command}`);
 
-switch (userCommand)
+// if command is not do-what-it-says
+// construct the parameter
+let parameter;
+if (command !== 'do-what-it-says')
+{
+  parameter = process.argv.slice(3).join(' ');
+  console.log(`parameter: ${parameter}`);
+}
+
+switch (command)
 {
   case 'concert-this':
-    concertThis();
+    concertThis(parameter);
     break;
   case 'spotify-this-song':
-    spotifyThisSong();
+    spotifyThisSong(parameter);
     break;
   case 'movie-this':
-    movieThis();
+    movieThis(parameter);
     break;
   case 'do-what-it-says':
     doWhatItSays();
@@ -40,15 +57,15 @@ switch (userCommand)
     return;
 }
 
-function concertThis()
+function concertThis(parameter)
 {
 }
 
-function spotifyThisSong()
+function spotifyThisSong(parameter)
 {
 }
 
-function movieThis()
+function movieThis(parameter)
 {
 }
 
