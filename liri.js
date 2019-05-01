9cc3e1a36efb86c4e8b379e9e3ddf66b8fb2a7ee
John Michael Laceda <john.laceda@gmail.com>
2019-05-01 16:32:09 -0700

    implement logging

--- liri.js
+++ liri.js
@@ -26,12 +26,12 @@ const command = process.argv[2];
 // when user gives a command not in the list.
 if (validCommands.indexOf(command) === -1)
 {
-  console.error('invalid command');
+  log('invalid command\n');
   return;
 }
 
 // at this point we have a legit command
-console.log(`command: ${command}`);
+log(`command: ${command}\n`);
 
 // if command is not do-what-it-says
 // construct the parameter
@@ -39,7 +39,7 @@ let parameter;
 if (command !== 'do-what-it-says')
 {
   parameter = process.argv.slice(3).join(' ');
-  console.log(`parameter: ${parameter}`);
+  log(`parameter: ${parameter}\n`);
 }
 
 function processCommand(command, parameter)
@@ -59,7 +59,7 @@ function processCommand(command, parameter)
       doWhatItSays();
       break;
     default:
-      console.error('unable to interpret command');
+      log('unable to interpret command\n');
       return;
   }
 }
@@ -69,21 +69,21 @@ processCommand(command, parameter);
 
 function concertThis(artist)
 {
-  console.log(`doing concertThis on ${artist}`);
+  log(`doing concertThis on ${artist}\n`);
   axios.get(`https://rest.bandsintown.com/artists/${encodeURI(artist)}/events?app_id=${keys.bandsintown.apiKey}`)
     .then((response) =>
     {
       response.data.forEach((concert) =>
       {
-        console.log(`
+        log(`
 Venue:   ${concert.venue.name}
 City:    ${concert.venue.city}, ${concert.venue.region||concert.venue.country}
-Date:    ${moment(concert.datetime).format('MM/DD/YYYY')}`);
+Date:    ${moment(concert.datetime).format('MM/DD/YYYY')}\n`);
       });
     })
     .catch((error) =>
     {
-      console.error('error getting concerts', error);
+      log('error getting concerts', error, '\n');
     });
 };
 
@@ -93,21 +93,21 @@ function spotifyThisSong(song)
   {
     song = 'The Sign Ace of Base';
   }
-  console.log(`doing spotifyThisSong on ${song}`);
+  log(`doing spotifyThisSong on ${song}\n`);
   spotify
     .search({ type: 'track', query: song })
     .then((response) =>
     {
       const firstTrack = response.tracks.items[0];
-      console.log(`
+      log(`
 Artist:       ${firstTrack.artists[0].name}
 Song Name:    ${firstTrack.name}
 Preview Link: ${firstTrack.preview_url||firstTrack.external_urls.spotify}
-Album:        ${firstTrack.album.name}`);
+Album:        ${firstTrack.album.name}\n`);
     })
     .catch((error) =>
     {
-      console.log('error getting song info', error);
+      log('error getting song info', error, '\n');
     });
 }
 
@@ -117,7 +117,7 @@ function movieThis(movie)
   {
     movie = "Mr. Nobody"
   }
-  console.log(`doing movieThis on ${movie}`);
+  log(`doing movieThis on ${movie}\n`);
 
   // * Title of the movie.
   // * Year the movie came out.
@@ -132,7 +132,7 @@ function movieThis(movie)
     .then((response) =>
     {
       response.data
-      console.log(`
+      log(`
 Title:       ${response.data.Title}
 Year:        ${response.data.Year}
 IMBD Rating: ${response.data.Ratings.filter(r => r.Source === 'Internet Movie Database')[0].Value}
@@ -140,11 +140,11 @@ RT Rating:   ${response.data.Ratings.filter(r => r.Source === 'Rotten Tomatoes')
 Country:     ${response.data.Country}
 Language:    ${response.data.Language}
 Actors:      ${response.data.Actors}
-Plot:        ${response.data.Plot}`);
+Plot:        ${response.data.Plot}\n`);
     })
     .catch((error) =>
     {
-      console.log('error getting movie info', error);
+      log('error getting movie info', error, '\n');
     });
 
 }
@@ -165,5 +165,13 @@ function doWhatItSays()
     // take the quotes out of the parameters
     const parameter = lineArr[1].slice(1,-1);
     processCommand(command, parameter);
+  });
+}
+
+function log(message)
+{
+  fs.appendFile('log.txt', message, (err) => {
+    if (err) throw err;
+    console.log(message);
   });
 }
